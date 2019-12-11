2ec98ce631cd7ee81e26a8bf01e4c8ba0551850a
Nobel <iamtribulation@outlook.com>
2019-12-11 22:46:55 +0545

    Adding registration route

--- src/store.js
+++ src/store.js
@@ -9,6 +9,7 @@ export default new Vuex.Store({
     items: [ 
       { text: 'Home', to: '/', icon:'home'},
       { text: 'Events', to: '/events', icon: 'rounded_corner'},
+      { text: 'Registration', to: '/registration', icon: 'rounded_corner'},
       { text: 'Team', to: '/team', icon:'group'},
       { text: 'About', to: '/about', icon: 'toc'},
       { text: 'Contact', to: '/contact', icon:'person'}
